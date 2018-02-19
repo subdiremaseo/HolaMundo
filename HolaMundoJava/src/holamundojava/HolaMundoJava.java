e5abd67d441509faadc2435342361c27b18e3cab
subdiremaseo <36480941+subdiremaseo@users.noreply.github.com>
2018-02-19 16:09:09 -0500

    Update HolaMundoJava.java

--- HolaMundoJava/src/holamundojava/HolaMundoJava.java
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -17,6 +17,7 @@ public class HolaMundoJava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("nuevo texto");
     }
     
 }
